498cc590a628173fd6fa5b45f80b02d302ee47f0
Omar Alshammari <omar.alshammary@hotmail.com>
2024-05-12 15:19:03 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Omar Alshammari on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
